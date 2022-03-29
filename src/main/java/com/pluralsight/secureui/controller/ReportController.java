cc1ec042c64a3f59e2c92f366be099017dc82064
ags-strive <aspencer@striveconsulting.com>
2022-03-29 14:26:42 -0500

    added config & bean for resource server

--- src/main/java/com/pluralsight/secureui/controller/ReportController.java
+++ src/main/java/com/pluralsight/secureui/controller/ReportController.java
@@ -1,11 +1,20 @@
 package com.pluralsight.secureui.controller;
 
+import com.pluralsight.secureui.data.TollData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @Controller
 public class ReportController {
+
+    @Autowired
+    private WebClient webClient;
 
     @RequestMapping("/")
     public String loadHome() {
@@ -14,6 +23,21 @@ public class ReportController {
 
     @RequestMapping("/report")
     public String loadReport(Model m) {
+        m.addAttribute("tolldata", collectTollData());
+
         return "report";
+    }
+
+    private List<TollData> collectTollData() {
+        // call downstream service
+        Flux<TollData> response = this.webClient.get()
+                .uri("http://localhost:8081/api/tolldata")
+                .retrieve()
+                .bodyToFlux(TollData.class);
+
+        List<TollData> temp = response.collectList().block();
+        System.out.println("HERE: " + temp.get(0).getLicensePlate());
+
+        return temp;
     }
 }
